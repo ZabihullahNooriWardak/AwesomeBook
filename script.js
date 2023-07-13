9c01dc8a5a0196c2cdf2f8f96473a8bd4596d846
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-07-13 21:24:28 +0430

    method added inside class

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ const addButton = document.querySelector('.add');
 
 // list for all books
 let allBooks = [];
-const arrayFromLocalStorage = localStorage.getItem('allBooks');
-if (arrayFromLocalStorage !== null) {
-  const arrayOfAllbooksFromLocal = JSON.parse(arrayFromLocalStorage);
-  allBooks = arrayOfAllbooksFromLocal;
-  for (let i = 0; i < allBooks.length; i += 1) {
-    const containerForSingleBook = document.createElement('div');
-    containerForSingleBook.classList.add('containerBooks');
-    containerForSingleBook.innerHTML = `<div class=bookDetails><p id='bookName'>${allBooks[i].bookName}</p> <p id='by'>by</p> <p id='bookAuthor'>${allBooks[i].bookAuthor}</p></div><button class="remove">remove</button>`;
-    containerForAllBooks.appendChild(containerForSingleBook);
-  }
-}
+
 // class for single book
 class SingleBook {
   constructor(name, author) {
@@ -35,26 +25,43 @@ class SingleBook {
     bookTextField.value = '';
     authorTextField.value = '';
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  display() {
+    const arrayFromLocalStorage = localStorage.getItem('allBooks');
+    if (arrayFromLocalStorage !== null) {
+      const arrayOfAllbooksFromLocal = JSON.parse(arrayFromLocalStorage);
+      allBooks = arrayOfAllbooksFromLocal;
+      for (let i = 0; i < allBooks.length; i += 1) {
+        const containerForSingleBook = document.createElement('div');
+        containerForSingleBook.classList.add('containerBooks');
+        containerForSingleBook.innerHTML = `<div class=bookDetails><p id='bookName'>${allBooks[i].bookName}</p> <p id='by'>by</p> <p id='bookAuthor'>${allBooks[i].bookAuthor}</p></div><button class="remove">remove</button>`;
+        containerForAllBooks.appendChild(containerForSingleBook);
+      }
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleRemoveButtonClick(event) {
+    if (event.target.classList.contains('remove')) {
+      const removeButton = event.target;
+      const bookContainer = removeButton.parentNode;
+      const index = Array.from(containerForAllBooks.children).indexOf(bookContainer);
+      if (index > -1) {
+        allBooks.splice(index, 1);
+        localStorage.setItem('allBooks', JSON.stringify(allBooks));
+        containerForAllBooks.removeChild(bookContainer);
+      }
+    }
+  }
 }
+new SingleBook().display();
 // Function for adding new Book
 
 // remove button function
 
-function handleRemoveButtonClick(event) {
-  if (event.target.classList.contains('remove')) {
-    const removeButton = event.target;
-    const bookContainer = removeButton.parentNode;
-    const index = Array.from(containerForAllBooks.children).indexOf(bookContainer);
-    if (index > -1) {
-      allBooks.splice(index, 1);
-      localStorage.setItem('allBooks', JSON.stringify(allBooks));
-      containerForAllBooks.removeChild(bookContainer);
-    }
-  }
-}
-
 // Attach the remove button click event listener to the container
-containerForAllBooks.addEventListener('click', handleRemoveButtonClick);
+containerForAllBooks.addEventListener('click', new SingleBook().handleRemoveButtonClick);
 // add button click  eventHandler
 addButton.addEventListener('click', () => {
   if (bookTextField.value !== '' && authorTextField.value !== '') {
