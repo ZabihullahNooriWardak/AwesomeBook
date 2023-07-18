96f3aaffe1ae43e3b6929f0a7458ca28a6f6c07f
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-07-18 15:28:29 +0430

    modularization completed

---
+++ modules/dom.js
@@ -0,0 +1,14 @@
+
+// Dom manipulation
+export { containerForAllBooks,bookTextField,authorTextField,addButton,addNew,contact,list,containerAllbooks,inputForNewBook,contactContainer};
+ const containerForAllBooks = document.querySelector('.container');
+ const bookTextField = document.querySelector('.bookName');
+ const authorTextField = document.querySelector('.bookAuthor');
+ const addButton = document.querySelector('.add');
+// Dom manipulation for the header Menu items
+ const addNew = document.getElementById('addNew');
+ const contact = document.getElementById('contact');
+ const list = document.getElementById('list');
+ const containerAllbooks = document.querySelector('div.container');
+ const inputForNewBook = document.querySelector('div.inputs');
+ const contactContainer = document.querySelector('div.contact');
